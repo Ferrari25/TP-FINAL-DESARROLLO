9de637b62ea3615f899900de23fee3365debb545
Ferrari25 <santiagoferrari.02@gmail.com>
2024-11-09 06:57:59 -0300

    finish add curso

--- Front-TPFINAL2/src/app/components/curso/curso-add/curso-add.component.ts
+++ Front-TPFINAL2/src/app/components/curso/curso-add/curso-add.component.ts
@@ -1,10 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {Curso} from "../../../models/curso.model";
 import {CursoService} from "../../../services/curso.service";
-import {FormsModule} from "@angular/forms";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
 import {DatePipe, NgForOf} from "@angular/common";
 import {Estudiante} from "../../../models/estudiante.model";
 import {EstudianteService} from "../../../services/estudiante.service";
+import {TemaService} from "../../../services/tema.service";
+import {DocenteService} from "../../../services/docente.service";
+import {Route, Router} from "@angular/router";
 
 @Component({
   selector: 'app-curso-add',
@@ -12,46 +23,73 @@ import {EstudianteService} from "../../../services/estudiante.service";
   imports: [
     FormsModule,
     DatePipe,
-    NgForOf
+    NgForOf,
+    ReactiveFormsModule
   ],
   templateUrl: './curso-add.component.html',
   styleUrl: './curso-add.component.css'
 })
 export class CursoAddComponent implements OnInit {
 
-  curso: Curso = new Curso(0, {id: 0, nombre: '', descripcion: ''}, new Date(), new Date(), {id: 0, nombre: '', legajo: 0}, 0, []);
-  estudiantes: Estudiante[] = [];
-  estudianteSeleccionado: Estudiante | undefined;
+  cursoForm: FormGroup;
+  docentes: any[] = [];
+  estudiantes: any[] = [];
+  temas: any[] = [];
 
-  constructor(private cursoService: CursoService, private estudianteService: EstudianteService) {}
-
-  ngOnInit(): void {
-    this.estudianteService.getEstudiantesAll().subscribe((data: Estudiante[]) => {
-      this.estudiantes = data;
+  constructor(
+    private fb: FormBuilder,
+    private cursoService: CursoService,
+    private docenteService: DocenteService,
+    private estudianteService: EstudianteService,
+    private temaService: TemaService,
+    private router: Router
+  ) {
+    this.cursoForm = this.fb.group({
+      tema: this.fb.group({
+        id: ['', Validators.required]
+      }),
+      fechaInicio: ['', Validators.required],
+      fechaFin: ['', Validators.required],
+      docente: this.fb.group({
+        id: ['', Validators.required]
+      }),
+      precio: ['', Validators.required],
+      estudiantes: this.fb.array([])
     });
   }
 
-  agregarEstudiante(): void {
-    if (this.estudianteSeleccionado && !this.curso.estudiantes.some(e => e.id === this.estudianteSeleccionado?.id)) {
-      this.curso.estudiantes.push(this.estudianteSeleccionado);
+  ngOnInit(): void {
+    this.docenteService.getDocentesAll().subscribe(data => {
+      this.docentes = data;
+    });
+
+    this.estudianteService.getEstudiantesAll().subscribe(data => {
+      this.estudiantes = data;
+    });
+
+    this.temaService.getTemasAll().subscribe(data => {
+      this.temas = data;
+    });
+  }
+
+  onCheckboxChange(event: any, estudiante: any) {
+    const estudiantesArray = this.cursoForm.get('estudiantes') as FormArray;
+    if (event.target.checked) {
+      estudiantesArray.push(new FormGroup({
+        id: new FormControl(estudiante.id)
+      }));
+    } else {
+      const index = estudiantesArray.controls.findIndex(x => x.value.id === estudiante.id);
+      estudiantesArray.removeAt(index);
     }
   }
 
-  eliminarEstudiante(index: number): void {
-    this.curso.estudiantes.splice(index, 1);
-  }
-
   onSubmit() {
-    console.log('Curso a enviar:', this.curso); // Verificar el contenido del curso
-    this.cursoService.saveCurso(this.curso).subscribe(
-      (response) => {
-        console.log('Curso guardado:', response); // Verificar la respuesta del backend
-        alert('Curso agregado exitosamente');
-        this.curso = new Curso(0, {id: 0, nombre: '', descripcion: ''}, new Date(), new Date(), {id: 0, nombre: '', legajo: 0}, 0, []);
-      },
-      (error) => {
-        console.error('Error al guardar el curso:', error); // Manejar errores
-      }
-    );
+    if (this.cursoForm.valid) {
+      this.cursoService.saveCurso(this.cursoForm.value).subscribe(response => {
+        this.router.navigate(['/curso/view-curso']);
+        console.log('CURSO CREADO:', response);
+      });
+    }
   }
 }
